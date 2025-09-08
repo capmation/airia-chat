785621f010a89845c7dc2e863833505fbe48014e
Nicolas Vela <nicolasvelah@gmail.com>
2025-09-08 14:42:16 -0600

    login loading and styles details

--- src/app/components/Login.jsx
+++ src/app/components/Login.jsx
@@ -5,24 +5,28 @@ import { api } from "../../utils/api";
 export default function Login({ onLoggedIn }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
   async function handleSubmit(e) {
     e.preventDefault();
     setError("");
     try {
+      setLoading(true);
       const { data } = await api.post("/api/auth/login", { username, password });
       localStorage.setItem("token", data.token);
       onLoggedIn?.();
     } catch (err) {
       setError("Invalid credentials");
+    } finally {
+      setLoading(false);
     }
   }
 
   return (
     <div className="max-w-sm mx-auto p-6 rounded-xl bg-white shadow">
       <img
-          className="w-1/2 sm:w-1/4 m-auto"
+          className="w-1/2 m-auto"
           alt="Capmation logo"
           src="/capmation-agents-logo.png"
         />
@@ -42,7 +46,16 @@ export default function Login({ onLoggedIn }) {
           onChange={(e) => setPassword(e.target.value)}
         />
         {error && <p className="text-red-600 text-sm">{error}</p>}
-        <button className="w-full rounded px-3 py-2 border">Login</button>
+        <button type="submit"
+          className="w-full rounded px-3 py-2 border cursor-pointer" 
+          disabled={loading}
+        >
+          { !loading 
+            ? (<span>Login</span>)
+            : (<div class="animate-spin rounded-full h-6 w-6 border-t-4 border-amber-500 m-auto"></div>)
+          }
+        </button>
+        
       </form>
     </div>
   );
